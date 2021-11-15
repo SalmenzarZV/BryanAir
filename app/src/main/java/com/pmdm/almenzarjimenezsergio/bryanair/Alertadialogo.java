8e0ea711fb199df2cb22be5106dee37ed81fce59
SergioAlmenzarIESZaidinVergeles20-21 <salmenzar1510@ieszaidinvergeles.org>
2021-11-15 19:16:57 +0100

    alertadialogo?

--- app/src/main/java/com/pmdm/almenzarjimenezsergio/bryanair/Alertadialogo.java
+++ app/src/main/java/com/pmdm/almenzarjimenezsergio/bryanair/Alertadialogo.java
@@ -3,11 +3,11 @@ package com.pmdm.almenzarjimenezsergio.bryanair;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 public class Alertadialogo extends DialogFragment {
@@ -17,26 +17,19 @@ public class Alertadialogo extends DialogFragment {
         this.bundle = bundle;
     }
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         builder.setMessage(R.string.alertDialogMessage).setTitle(R.string.alertDialogTitle)
-                .setPositiveButton(R.string.aceptar, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Context context = getContext();
-                        Intent intent = new Intent(context, BillActivity.class);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-                    }
+                .setPositiveButton(R.string.aceptar, (dialogInterface, i) -> {
+                    Context context = getContext();
+                    Intent intent = new Intent(context, BillActivity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
                 })
-                .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
+                .setNegativeButton(R.string.cancelar, (dialogInterface, i) -> dialogInterface.cancel());
 
         return  builder.create();
     }
